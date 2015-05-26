a539c9193c2c4727984bdfcb93643b1def8f7c1d
juliusray <juliusray.inocencio@gmail.com>
2015-05-26 15:26:54 -0700

    added main.cpp in repo

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	string greeting = "hello ";
+	string repo_name = "Github";
+	
+	cout << greeting << repo_name;
+	
+	return 0;
+
+}
